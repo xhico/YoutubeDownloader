ce757496b839404936a985c0384cc168f0ca336e
xhico <francgf@gmail.com>
2022-01-03 02:45:23 +0000

    Retry for Failed Videos

--- PlaylistDownloader.py
+++ PlaylistDownloader.py
@@ -13,6 +13,53 @@ import sys
 from pytube import Playlist, YouTube
 
 
+def download(vodURL, folderPath, idx, numbTotal):
+    # Get video
+    try:
+        video = YouTube(vodURL)
+    except Exception:
+        print("ERROR - Couldn't reach video - " + vodURL)
+        return False
+
+    # Set filename, fullPath and temporary video/audio files
+    # Set titles, allow only Alphanumeric and " .,_-" -> Remove multi-spaces
+    title = re.sub('\s+', ' ', re.sub(r'[^A-Za-z0-9 .,_-]+', '', video.title))
+    filename = idx + " - " + title
+    fullPath = folderPath + filename + ".mp4"
+    tmpVideo = folderPath + "tmp_video"
+    tmpAudio = folderPath + "tmp_audio"
+
+    # Show progress
+    print("(" + str(round(int(idx) / int(numbTotal) * 100, 2)) + "% - " + str(idx) + "/" + str(numbTotal) + ") - " + title)
+
+    try:
+        # Download the best video quality
+        print("INFO - Downloading video")
+        video_only = video.streams.filter(type='video').order_by('resolution').last()
+        video_only.download(filename=tmpVideo)
+
+        # Download the best audio quality
+        print("INFO - Downloading audio")
+        audio_only = video.streams.filter(type='audio').order_by('abr').last()
+        audio_only.download(filename=tmpAudio)
+
+        # Merge two files
+        print("INFO - Merging video-audio")
+        cmd = "ffmpeg -i '" + tmpVideo + "' -i '" + tmpAudio + "' -c:v copy -c:a aac '" + fullPath + "' -loglevel error"
+        os.system(cmd)
+
+        # Remove tmp video/audio files
+        os.remove(tmpVideo)
+        os.remove(tmpAudio)
+
+        print("INFO - Download Successful")
+    except Exception:
+        print("ERROR - Couldn't download Video, check URL")
+        return False
+
+    return True
+
+
 def main(playlistURL, folderPath):
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,9 +67,10 @@ def main(playlistURL, folderPath):
     try:
         p = Playlist(playlistURL)
     except Exception:
-        print("Couldn't reach Playlist, check URL")
+        print("ERROR - Couldn't reach Playlist - " + playlistURL)
+        return
 
-    # Set number of total videos and numbering format
+        # Set number of total videos and numbering format
     numbTotal = len(p.video_urls)
     formatIdx = "{:0" + str(len(str(numbTotal))) + "}"
 
@@ -34,54 +82,51 @@ def main(playlistURL, folderPath):
     # Start from oldest to newest
     video_urls = reversed(p.video_urls)
 
+    # Set Failed Video
+    failedVideos, failedVideosAgain = {}, {}
+
     # Iterate over every video URL
     for idx, vodURL in enumerate(video_urls):
-        # Get video
-        try:
-            video = YouTube(vodURL)
-        except Exception:
-            print("ERROR - Couldn't reach Video, check URL\n")
-            continue
-
         # Set index format (001, 002, 003, ..., 323)
         idx = formatIdx.format(idx + 1)
 
-        # Set filename, fullPath and temporary video/audio files
-        # Set titles, allow only Alphanumeric and " .,_-" -> Remove multi-spaces
-        title = re.sub('\s+', ' ', re.sub(r'[^A-Za-z0-9 .,_-]+', '', video.title))
-        filename = idx + " - " + title
-        fullPath = folderPath + filename + ".mp4"
-        tmpVideo = folderPath + "tmp_video"
-        tmpAudio = folderPath + "tmp_audio"
+        # Download Video
+        status = download(vodURL, folderPath, idx, numbTotal)
 
-        # Show progress
-        print("(" + str(round(int(idx) / numbTotal * 100, 2)) + "% - " + idx + "/" + str(numbTotal) + ") - " + title)
-
-        try:
-            # Download the best video quality
-            print("Downloading video")
-            video_only = video.streams.filter(type='video').order_by('resolution').last()
-            video_only.download(filename=tmpVideo)
-
-            # Download the best audio quality
-            print("Downloading audio")
-            audio_only = video.streams.filter(type='audio').order_by('abr').last()
-            audio_only.download(filename=tmpAudio)
-        except Exception as ex:
-            print("ERROR - Couldn't download files\n")
-            continue
-
-        # Merge two files
-        print("Merging vidio-audio")
-        cmd = "ffmpeg -i '" + tmpVideo + "' -i '" + tmpAudio + "' -c:v copy -c:a aac '" + fullPath + "' -loglevel error"
-        os.system(cmd)
-
-        # Remove tmp video/audio files
-        os.remove(tmpVideo)
-        os.remove(tmpAudio)
+        # Check for failed status
+        if status is False:
+            failedVideos[idx] = vodURL
 
         print("")
-    return
+
+    # Check for Failed Videos
+    print("--------------------------------")
+    if len(failedVideos) != 0:
+        totalFailed = formatIdx.format(len(failedVideos))
+        print("INFO - Failed videos: " + totalFailed)
+        print("INFO - Retrying")
+        print("--------------------------------\n")
+
+        # Retry for failed Videos
+        for idx, vodURL in failedVideos.items():
+            status = download(vodURL, folderPath, idx, totalFailed)
+            if status is False:
+                failedVideosAgain[idx] = vodURL
+
+            print("")
+
+    # Create log file for unsuccessful videos
+    if len(failedVideosAgain) != 0:
+        with open(folderPath + "log.txt", "w") as logFile:
+            for idx, vodURL in failedVideosAgain.items():
+                logFile.write(idx + " - " + vodURL + "\n")
+        logFile.close()
+
+    print("--------------------------------")
+    print("INFO - Finished Downloading")
+    print("INFO - Failed Videos: " + formatIdx.format(len(failedVideosAgain)))
+    print("INFO - Location:" + sys.argv[2])
+    return True
 
 
 if __name__ == '__main__':
@@ -90,3 +135,4 @@ if __name__ == '__main__':
         exit()
 
     main(sys.argv[1], sys.argv[2])
+    exit()
